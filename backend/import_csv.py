9a108729f31ec05d8146dcf3e825fc3559e73417
Алексей Московцев <alexmosgalaxy@gmail.com>
2025-01-03 11:36:30 +0300

    +

--- backend/import_csv.py
+++ backend/import_csv.py
@@ -2,26 +2,31 @@ import os
 import django
 import csv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_backend.settings')
-django.setup()
 
-from api.models import Ingredient, Tag
+def import_ingredients():
+    csv_file_path = 'ingredients.csv'
+    with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            Ingredient.objects.get_or_create(
+                name=row['name'], measurement_unit=row['measurement_unit']
+            )
+            print(f"Добавлен ингредиент: {row['name']}")
 
 
-csv_file_path = 'ingredients.csv'
+def import_tags():
+    csv_file_path = 'tags.csv'
+    with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            Tag.objects.get_or_create(name=row['name'], slug=row['slug'])
+            print(f"Добавлен тег: {row['name']}")
 
-with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
-    reader = csv.DictReader(csv_file)
-    for row in reader:
-        Ingredient.objects.get_or_create(
-            name=row['name'], measurement_unit=row['measurement_unit']
-        )
-        print(f"Добавлен ингредиент: {row['name']}")
 
-csv_file_path = 'tags.csv'
-
-with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
-    reader = csv.DictReader(csv_file)
-    for row in reader:
-        Tag.objects.get_or_create(name=row['name'], slug=row['slug'])
-        print(f"Добавлен тег: {row['name']}")
+if __name__ == "__main__":
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'foodgram_backend.settings')
+    django.setup()
+    from api.models import Ingredient, Tag
+    import_ingredients()
+    import_tags()
